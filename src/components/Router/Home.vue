5f8031d7d6759b5c357201af674a0f3324f28e02
levon <levon.gambaryan@gmail.com>
2019-09-22 19:55:52 +0300

    add design scripts to mainpage

--- src/components/Router/Home.vue
+++ src/components/Router/Home.vue
@@ -1,7 +1,43 @@
+<!-- eslint-disable max-len -->
 <template>
   <div class="home">
-    <Loading />
-    <footer class="footer layout__footer" role="contentinfo">
+    <div class="background background-0 hidden-xs hidden-sm"></div>
+    <div id="background" class="background background-1 hidden-xs hidden-sm"></div>
+    <div id="scene-1" class="scene-1 hidden-xs hidden-sm">
+      <div data-depth="0.1" class="scene-1__1">
+      </div>
+      <div data-depth="0.4" originX="" class="scene-1__2">
+        <h2><strong>Kafedra</strong> of experimental psychonautics<span class="visible-lg-inline"> </span><br class="hidden-lg" />and inner space research</h2>
+      </div>
+      <div data-depth="0" originX="" class="scene-1__3">
+        <div class="boom"><div class="pulse"><span>Radio! New!</span></div></div>
+        <div class="arrow"><span class="bounce icon icon-down"></span></div>
+      </div>
+    </div>
+    <div class="scene-1-gadgets visible-xs visible-sm">
+      <h2><strong>Kafedra</strong> of experimental psychonautics<br />and inner space research</h2>
+    </div>
+    <div id="scene-2" class="scene-2">
+      <div class="player">
+        <div class="player__now">
+          <div class="player__label">Now</div>
+          <div class="player__track-title">No Artist - No Title</div>
+          <div class="player__duration">0:00</div>
+        </div>
+        <div class="player__next">
+          <div class="player__label">Next</div>
+          <div class="player__track-title">No Artist - No Title</div>
+          <div class="player__duration">0:00</div>
+        </div>
+        <div class="player__btns">
+          <a href="#" class="btn btn--play"><span class="btn__text">Play<i class="fas fa-play"></i></span></a
+          ><a href="#" class="btn btn--pause"><span class="btn__text">Pause<i class="fas fa-pause"></i></span></a
+          ><a href="#" class="btn btn--next"><span class="btn__text">Next<i class="fas fa-step-forward"></i></span></a>
+        </div>
+      </div>
+      <div id="radio"></div>
+    </div>
+    <footer class="footer home__footer" role="contentinfo">
       <address>
         <ul class="footer__links">
           <li>
@@ -24,22 +60,86 @@
 </template>
 
 <script>
-import Loading from '@/components/Utils/Loading.vue';
+import Parallax from 'parallax-js';
+
+// mport path from 'path'; // for logger
+// import Logger from '@/utils/logger';
+import ScreenHelper from '@/utils/_screen-helper';
+
+// const logger = new Logger(path.basename(__filename, '.js'));
+
+// import Loading from '@/components/Utils/Loading.vue';
+
+const getHeight = () => {
+  const scene = document.getElementById('scene-1');
+  return Number(window.getComputedStyle(scene, null).getPropertyValue('height').slice(0, -2));
+};
+
+const checkScroll = (scroll) => {
+  // logger.info('checkScroll');
+  const background = document.getElementById('background');
+  const radio = document.getElementById('radio');
+  if (ScreenHelper.isDesktop()) {
+    const top = -0.75 * (scroll - 10);
+    background.style.top = `${top}px`;
+
+    if (scroll > getHeight() - 300) {
+      radio.classList.add('riseUp');
+      radio.classList.remove('riseDown');
+    } else {
+      radio.classList.remove('riseUp');
+      radio.classList.add('riseDown');
+    }
+  } else {
+    radio.classList.remove('riseUp');
+    radio.classList.remove('riseDown');
+  }
+};
+
+const onscroll = () => {
+  // logger.info('onscroll');
+  const scroll = window.pageYOffset || document.documentElement.scrollTop;
+  checkScroll(scroll);
+  return scroll;
+};
+
+const redraw = () => {
+  // logger.info('redraw');
+  if (ScreenHelper.isDesktop()) {
+    const radio = document.getElementById('radio');
+    const height = getHeight();
+    let h;
+    if (height > 700) {
+      h = (height - 60) * 0.6;
+    } else {
+      h = (height - 60) * 0.5;
+    }
+    radio.style.height = `${h}px`;
+  } else {
+    radio.style.height = '';
+  }
+  checkScroll(onscroll());
+};
 
 export default {
   name: 'Home',
 
   components: {
-    Loading,
   },
 
   beforeCreate() {
     // this.$store.dispatch('utils/TEST_ACTION');
   },
+
+  mounted() {
+    const scene = document.getElementById('scene-1');
+    const parallaxInstance = new Parallax(scene); // eslint-disable-line no-unused-vars
+
+    window.addEventListener('resize', () => redraw());
+    window.addEventListener('scroll', () => onscroll());
+
+    redraw();
+  },
 };
 </script>
-
-<style lang="scss" scoped>
-  @import "@/styles/_stylebase.scss";
-
-</style>
+<!-- eslint-enable max-len -->
